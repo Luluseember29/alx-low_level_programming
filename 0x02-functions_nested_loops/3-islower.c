10211b569a032e363b96839f8f5ed75a71960204
Luluseember29 <iorliamslucy@gmail.com>
2022-12-14 16:02:58 -0800

    o putchar

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * islower - Checks for lowercase character
  * @c: The character to be checked
  * Return: 1 for lowercase characteror 0 for anything else
  */
-
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
